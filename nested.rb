0509e4df538211ca71a066e0c05a7692495e0a66
Hal H Nader <halnader89@gmail.com>
2020-06-28 20:45:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,20 +20,15 @@ end
 def dennis_ritchies_language
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+      :dennis_ritchie => {
+      :known_for => "Unix",
+      :languages => ["C"]
      }
+    }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
