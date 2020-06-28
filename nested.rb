4b1ffb6dc61761ab9c15b931c4ccc398a58cc995
Hal H Nader <halnader89@gmail.com>
2020-06-28 20:30:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -5,11 +5,11 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
+        #:alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
+        #:dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
