b7a7b16964070885b2214c030d71e3df8177a80a
Hal H Nader <halnader89@gmail.com>
2020-06-28 20:42:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,6 @@
 def hopper
 	programmer_hash = 
  		{
-        
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
  		}
@@ -12,19 +11,9 @@ def alan_kay_is_known_for
 	
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
+        
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+          }
 end
 
 def dennis_ritchies_language
